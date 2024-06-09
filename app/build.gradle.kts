f39c5601de7047c4e84b8993b1459f1ca6a29e46
monticelli.ricardo <monticelli19@gmail.com>
2024-06-09 17:32:32 +0200

    Working build gradle files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
     id("com.google.devtools.ksp")
-    id("androidx.navigation.safeargs")
+    id("androidx.navigation.safeargs.kotlin")
     kotlin("kapt")
 }
 
@@ -31,11 +31,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
     buildFeatures {
         dataBinding = true
         viewBinding = true
     }
-
 }
 
 dependencies {
@@ -43,30 +45,33 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.compose.preview.renderer)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     // ViewModel
     implementation(libs.viewmodel)
-    // ViewModel utilities for Compose
-    implementation(libs.viewmodel.compose)
+    implementation(libs.viewmodel.savedstate)
     // LiveData
     implementation(libs.livedata)
-    // Lifecycles only (without ViewModel or LiveData)
+    // Lifecycles
     implementation(libs.lifecycle)
-    // Lifecycle utilities for Compose
     implementation(libs.lifecycle.compose)
-    // Annotation processor
-    annotationProcessor(libs.lifecycle.compiler)
+    kapt(libs.lifecycle.compiler)
+
     // ROOM Database
     implementation(libs.room)
-    annotationProcessor(libs.room.compiler)
+    kapt(libs.room.compiler)
+
     // Navigation
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     // Coroutines
     implementation(libs.coroutines)
-    implementation (libs.room.ktx)
+    implementation(libs.room.ktx)
+
+    // Dagger
+    implementation("com.google.dagger:dagger:2.40")
+    kapt("com.google.dagger:dagger-compiler:2.40")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
+        mavenCentral()
         google()
+
     }
     dependencies {
-        val navVersion = "2.7.7"
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        classpath(libs.navigation.plugin)
         //classpath(libs.navigation.plugin)
     }
 }
