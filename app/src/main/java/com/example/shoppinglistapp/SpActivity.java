5d8d35d0e1e6e6ab94fc790effa98d11f3b11900
monticelli.ricardo <monticelli19@gmail.com>
2024-06-20 14:01:46 +0200

    Loading screen (SpActivity) fixed.

--- app/src/main/java/com/example/shoppinglistapp/SpActivity.java
+++ app/src/main/java/com/example/shoppinglistapp/SpActivity.java
@@ -4,35 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sp);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // Splash Screen Handler
-        Handler handler = new Handler();
-        // Delay the Main Activity start
-        handler.postAtTime(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity( new Intent(SpActivity.this, MainActivity.class));
+                startActivity(new Intent(SpActivity.this, MainActivity.class));
                 finish();
             }
-        }, 3000);
-
+        }, 3000); // 3 seconds delay
     }
 }
